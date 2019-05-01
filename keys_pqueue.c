a0708cb3871cfd5a38c9a475e2c849ae1b6190b9
wenox <wenoxar@gmail.com>
2019-05-01 05:37:34 +0200

    sample keys priority array

---
+++ keys_pqueue.c
@@ -0,0 +1,22 @@
+#include <key_codes.h>
+
+/** sorted from the most often pressed key to least often */
+const unsigned short keys_pqueue[18] = { KEY_LMB,
+KEY_SPACE,
+KEY_BACK,
+KEY_RETURN,
+KEY_RMB,
+KEY_SHIFT,
+KEY_E,
+KEY_A,
+KEY_O,
+KEY_I,
+KEY_O,
+KEY_D,
+KEY_W,
+KEY_K,
+KEY_Z,
+KEY_1,
+KEY_2,
+KEY_3
+}; // rest to be filled, in proper priority order, after I get right amount of data from WhatPulse app
---
+++ keys_pqueue.h
@@ -0,0 +1,6 @@
+#ifndef KEYS_PQUEUE_H_INCLUDED
+#define KEYS_PQUEUE_H_INCLUDED
+
+extern const unsigned short keys_pqueue[18];
+
+#endif // KEYS_PQUEUE_H_INCLUDED
