af7b7872ae83b7b2ac33fb69a3dcbd1b3c51839e
wenox <wenoxar@gmail.com>
2019-05-03 20:06:56 +0200

    split recording into more functions

--- recording.c
+++ recording.c
@@ -10,37 +10,47 @@
 #define _GETKEY 2
 #define _SLEEP 3
 
+void add_cursor(struct f_queue **head, struct f_queue **tail, POINT P[2])
+{
+    P[1] = get_cursor();
+    if (P[0].x != P[1].x || P[0].y != P[1].y) {               // if current cursor pos != previous
+        add_function(head, tail, _GETCURSOR, P[1].x, P[1].y); // add it to the queue
+        P[0] = P[1];
+    }
+}
+
+void add_keypress(struct f_queue **head, struct f_queue **tail, int key_buff[2])
+{
+    key_buff[1] = get_keystroke();
+    if (key_buff[1] != key_buff[0] && key_buff[1] != 0)     // if there was keystroke
+        add_function(head, tail, _GETKEY, key_buff[1], -1); // then add it to the queue
+    key_buff[0] = key_buff[1];
+}
+
 /** returns true if newly added node is function of sleep type, false otherwise */
 bool is_prev_sleep_func(struct f_queue **head)
 {
     return (*head)->f_type == _SLEEP;
 }
 
+void add_sleep(struct f_queue **head, struct f_queue **tail, const int sleep_dur)
+{
+    Sleep(sleep_dur);
+    if (!is_prev_sleep_func(head))
+        add_function(head, tail, _SLEEP, sleep_dur, -1);
+    else
+        (*head)->f_args[0] += sleep_dur; // increment the node rather than add new one
+}
+
 /** keyboard/mouse recording engine */
 void record(struct f_queue **head, struct f_queue **tail, const int sleep_dur)
 {
-    int key_buff[2] = {-1, -1};     // buffer for curr and prev pressed key
-    POINT P[2] = {0};               // buffer for curr and prev cursor position
+    int key_buff[2] = {-1, -1};               // buffer for curr and prev pressed key
+    POINT cursor_buff[2] = {0};               // buffer for curr and prev cursor position
 
-    while(key_buff[1] != KEY_W) {   // stop recording when '3' is pressed
-        /** add cursor to queue */
-        P[1] = get_cursor();
-        if (P[0].x != P[1].x || P[0].y != P[1].y) { // if current cursor pos != previous
-            add_function(head, tail, _GETCURSOR, P[1].x, P[1].y); // add it to the queue
-            P[0] = P[1];
-        }
-
-        /** add keypress to queue */
-        key_buff[1] = get_keystroke();
-        if (key_buff[1] != key_buff[0] && key_buff[1] != 0)     // if there was keystroke
-            add_function(head, tail, _GETKEY, key_buff[1], -1); // then add it to the queue
-        key_buff[0] = key_buff[1];
-
-        /** add sleep to queue */
-        Sleep(sleep_dur);
-        if (!is_prev_sleep_func(head))
-            add_function(head, tail, _SLEEP, sleep_dur, -1);
-        else
-            (*head)->f_args[0] += sleep_dur; // if prev node is a sleep func, increment that node rather than add new one
+    while(key_buff[1] != KEY_W) {             // stop recording when '3' is pressed
+        add_cursor(head, tail, cursor_buff);
+        add_keypress(head, tail, key_buff);
+        add_sleep(head, tail, sleep_dur);
     }
 }
--- recording.h
+++ recording.h
@@ -1,8 +1,14 @@
 #ifndef RECORDING_H_INCLUDED
 #define RECORDING_H_INCLUDED
 
+void add_cursor(struct f_queue **head, struct f_queue **tail, POINT P[2]);
+
+void add_keypress(struct f_queue **head, struct f_queue **tail, int key_buff[2]);
+
 /** returns true if newly added node is sleep function type, false otherwise */
 bool is_prev_sleep_func(struct f_queue **head);
+
+void add_sleep(struct f_queue **head, struct f_queue **tail, const int sleep_dur);
 
 /** keyboard/mouse recording engine */
 void record(struct f_queue **head, struct f_queue **tail, const int sleep_dur);
