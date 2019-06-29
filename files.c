4b4fa15f851128e04d1300bc46f827fd473a436d
wenox <wenoxar@gmail.com>
2019-06-29 16:45:50 +0200

    reading file now handles errors

--- files.c
+++ files.c
@@ -12,16 +12,12 @@ void save_recording(struct f_queue *tail, char *file_name)
         return;
     }
 
-    int line_counter = 0;
     while (tail) {
         fprintf(f, "%d %d %d\n", tail->f_type, tail->f_args[0], tail->f_args[1]);
-        line_counter++;
         tail = tail->prev;
     }
 
-    fprintf(f, "num of nodes: %d\n", line_counter);
     fclose(f);
-    printf("all saved\n");
 }
 
 bool load_recording(struct f_queue **head, struct f_queue **tail, char *file_name)
@@ -31,11 +27,19 @@ bool load_recording(struct f_queue **head, struct f_queue **tail, char *file_nam
         return false;
     }
 
+    char buffer[256];
     int f_type, arg1, arg2;
-    while (fscanf(f, "%d %d %d", &f_type, &arg1, &arg2)) {
-        //printf("%d %d %d\n", f_type, arg1, arg2);
+    while (fgets(buffer, sizeof(buffer), f) != NULL) {
+        if (sscanf(buffer, "%d %d %d", &f_type, &arg1, &arg2) != 3)
+            goto error;
+
         add_function(head, tail, f_type, arg1, arg2);
     }
 
+    fclose(f);
     return true;
+
+error:
+    free_recording(&head, &tail);
+    return false;
 }
